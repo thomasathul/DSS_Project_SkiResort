a83be989635a706cdc63655482ceb3d1ba0b3982
Athul Thomas <thomasathul@gmail.com>
2024-03-10 20:19:20 -0400

    Add files via upload

---
+++ Client_part_2/src/main/java/org/example/model/Skier.java
@@ -0,0 +1,48 @@
+package org.example.model;
+
+public class Skier {
+	private int resortID;
+	private String seasonID;
+	private String dayID;
+	private int skierID;
+	private int liftID;
+	private int time;
+	
+	public int getResortID() {
+		return resortID;
+	}
+	public void setResortID(int resortID) {
+		this.resortID = resortID;
+	}
+	public String getSeasonID() {
+		return seasonID;
+	}
+	public void setSeasonID(String seasonID) {
+		this.seasonID = seasonID;
+	}
+	public String getDayID() {
+		return dayID;
+	}
+	public void setDayID(String dayID) {
+		this.dayID = dayID;
+	}
+	public int getSkierID() {
+		return skierID;
+	}
+	public void setSkierID(int skierID) {
+		this.skierID = skierID;
+	}
+	public int getLiftID() {
+		return liftID;
+	}
+	public void setLiftID(int liftID) {
+		this.liftID = liftID;
+	}
+	public int getTime() {
+		return time;
+	}
+	public void setTime(int time) {
+		this.time = time;
+	}
+	
+}
